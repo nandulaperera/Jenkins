dca173d8c0503b448f933e05a864766fcd148ba7
Nandula Perera <pereranandula1999@gmail.com>
2020-01-16 02:35:50 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator{
 	public static void main(String args[]){
 		int number1=Integer.valueOf(args[0]);
 		int number2=Integer.valueOf(args[1]);
-		int result=number1+number2;
-		System.out.println(number1+" + "+number2+" = "+result);
+		int result=number1-number2;
+		System.out.println(number1+" - "+number2+" = "+result);
 	}
 }
