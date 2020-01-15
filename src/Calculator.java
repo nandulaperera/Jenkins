12e9321e46d1f2e238363fbc5a4b38aeb9f3d874
Nandula Perera <pereranandula1999@gmail.com>
2020-01-16 02:45:59 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator{
-	public static void man(String args[]){
+	public static void main(String args[]){
 		int number1=Integer.valueOf(args[0]);
 		int number2=Integer.valueOf(args[1]);
 		int result=number1-number2;
